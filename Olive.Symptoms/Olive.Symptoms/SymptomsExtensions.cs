5b6f14301f89c0f3d3e377957b48ff1a7bba99ea
Matt <matt@geeks.ltd.uk>
2021-08-29 11:22:42 +0100

    update

--- Olive.Symptoms/Olive.Symptoms/SymptomsExtensions.cs
+++ Olive.Symptoms/Olive.Symptoms/SymptomsExtensions.cs
@@ -1,6 +1,7 @@
 ﻿namespace Olive
 {
     using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Http;
     using Olive.Symptoms;
 
     public static class SymptomsExtensions
@@ -9,7 +10,15 @@
             where T : Source, new()
         {
             @this.Map("/api/discover-symptoms",
-                app => app.Run(context => SymptomsMiddleware.Discover<T>(context)));
+                app => app.Run(async context =>
+                {
+                    if (context.Request.Param("token") != Config.GetOrThrow("Olive.Symptoms:Token"))
+                    {
+                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    }
+                    else
+                        await SymptomsMiddleware.Discover<T>(context);
+                }));
 
             return @this;
         }
