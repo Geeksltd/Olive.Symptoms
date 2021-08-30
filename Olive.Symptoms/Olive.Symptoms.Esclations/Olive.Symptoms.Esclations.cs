dd66a58cb03eb97ed92cbd418fecdbfdfaba9f04
Matt <matt@geeks.ltd.uk>
2021-08-30 13:25:05 +0100

    update

---
+++ Olive.Symptoms/Olive.Symptoms.Esclations/Olive.Symptoms.Esclations.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Olive.Symptoms.Esclations
+{
+    public static class Extensions
+    {
+        public static Symptom EsclateToCTO(this Symptom @this, TimeSpan when) => @this.EscalateTo(People.CTO, when);
+    }
+}
