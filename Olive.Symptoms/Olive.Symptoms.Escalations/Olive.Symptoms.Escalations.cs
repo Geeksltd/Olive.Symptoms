26011409b68575e44f66115802cc26adacf852f8
Matt <matt@geeks.ltd.uk>
2021-09-28 15:17:14 +0100

    update

--- Olive.Symptoms/Olive.Symptoms.Escalations/Olive.Symptoms.Escalations.cs
+++ Olive.Symptoms/Olive.Symptoms.Escalations/Olive.Symptoms.Escalations.cs
@@ -5,5 +5,7 @@ namespace Olive.Symptoms.Escalations
     public static class Extensions
     {
         public static Symptom EscalateToCTO(this Symptom @this, TimeSpan when) => @this.EscalateTo(People.CTO, when);
+
+        public static Symptom AssignToDevOps(this Symptom @this) => @this.ResponsibleAll("DevOps");
     }
 }
